4997bd6083ed6f53096a77b64171544753739528
Faisal Abushanab <33567969+FaisalJAbushanab@users.noreply.github.com>
2023-06-15 14:09:30 +0300

    main file updated to include all models code

--- src/main.py
+++ src/main.py
@@ -5,11 +5,194 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
-
-import os
-
 #Importing Files 
-data = pd.read_csv('EEG_Eye_State_Classification.csv')
+data = pd.read_csv('../input/eye-state-classification-eeg-dataset/EEG_Eye_State_Classification.csv')
 data.head()
+
+#Check and remove any outliers from the data. 
+from pandas import read_csv
+from numpy import mean
+from numpy import std
+from numpy import delete
+from numpy import savetxt
+values = data.values
+# step over each EEG column
+for i in range(values.shape[1] - 1):
+	# calculate column mean and standard deviation
+	data_mean, data_std = mean(values[:,i]), std(values[:,i])
+	# define outlier bounds
+	cut_off = data_std * 4
+	lower, upper = data_mean - cut_off, data_mean + cut_off
+	# remove too small
+	too_small = [j for j in range(values.shape[0]) if values[j,i] < lower]
+	values = delete(values, too_small, 0)
+	print('>deleted %d rows' % len(too_small))
+	# remove too large
+	too_large = [j for j in range(values.shape[0]) if values[j,i] > upper]
+	values = delete(values, too_large, 0)
+	print('>deleted %d rows' % len(too_large))
+# save the results to a new file
+savetxt('EEG_Eye_State_no_outliers.csv', values, delimiter=',')
+
+# visualize dataset without outliers
+from pandas import read_csv
+from matplotlib import pyplot
+# load the dataset
+data = read_csv('EEG_Eye_State_no_outliers.csv', header=None)
+# retrieve data as numpy array
+values = data.values
+# create a subplot for each time series
+pyplot.figure()
+for i in range(values.shape[1]):
+	pyplot.subplot(values.shape[1], 1, i+1)
+	pyplot.plot(values[:, i])
+pyplot.show()
+
+#Find the optimal number of K using elbow method. 
+from sklearn.cluster import KMeans 
+from scipy.spatial.distance import cdist
+import math
+distortions = []
+K = range(1,10)
+for k in K: 
+    kmeans_model = KMeans(n_clusters=k)
+    kmeans_model.fit(X)
+    distortions.append(sum(np.min(cdist(X,kmeans_model.cluster_centers_,"euclidean"),axis=1))/X.shape[0])
+
+    import matplotlib.pyplot as plt
+#Plot distortions againt K value
+print(K)
+# checking for min of K value
+print(np.min(distortions))
+#Plotting for change in K value
+plt.plot(K,distortions,'bx-')
+plt.xlabel('k')
+plt.ylabel('Distortion')
+plt.title('Optimal number of clusters using elbow method')
+plt.show() 
+
+#Train Test and Split 
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+#Import KNN Classifier
+from sklearn.neighbors import KNeighborsClassifier
+# metric = minkowski and p=2 is Euclidean Distance
+# metric = minkowski and p=1 is Manhattan Distance
+classifier = KNeighborsClassifier(n_neighbors=2, metric="minkowski",p=2)
+classifier.fit(X_train, y_train) #Fit the data
+
+import joblib
+
+# Assuming you have trained the KNN classifier and stored it in the 'classifier' variable
+
+# Save the model to a file
+joblib.dump(classifier, 'knn_model.pkl')
+
+#Make Prediction
+y_pred = classifier.predict(X_test)
+print(y_pred)
+
+# Step 7 - Confusion Matrix to test accuracy of our model 
+import seaborn as sns
+from sklearn import metrics
+cm = metrics.confusion_matrix(y_test, y_pred) 
+print(cm)
+accuracy = metrics.accuracy_score(y_test, y_pred) 
+print("Accuracy score:",accuracy)
+precision = metrics.precision_score(y_test, y_pred) 
+print("Precision score:",precision)
+recall = metrics.recall_score(y_test, y_pred) 
+print("Recall score:",recall)
+
+# Plotting the Confusion Matrix
+plt.figure(figsize=(8, 6))
+sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
+plt.title("Confusion Matrix")
+plt.xlabel("Predicted Labels")
+plt.ylabel("True Labels")
+plt.show()
+
+#Apply Logistic Regression and then test its accuracy
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+y_pred = logreg.predict(X_test)
+print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test, y_test)))
+
+#Confusion Matrix
+from sklearn.metrics import confusion_matrix
+confusion_matrix = confusion_matrix(y_test, y_pred)
+print(confusion_matrix)
+
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+logit_roc_auc = roc_auc_score(y_test, logreg.predict(X_test))
+fpr, tpr, thresholds = roc_curve(y_test, logreg.predict_proba(X_test)[:,1])
+plt.figure()
+plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc)
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic')
+plt.legend(loc="lower right")
+plt.savefig('Log_ROC')
+plt.show()
+
+from sklearn.naive_bayes import GaussianNB  
+classifier = GaussianNB()  
+classifier.fit(X_train, y_train) 
+
+y_pred = classifier.predict(X_test)  
+
+# Making the Confusion Matrix  
+from sklearn.metrics import confusion_matrix  
+import seaborn as sns
+cm = confusion_matrix(y_test, y_pred)  
+print(cm)
+accuracy = metrics.accuracy_score(y_test, y_pred) 
+print("Accuracy score:",accuracy)
+precision = metrics.precision_score(y_test, y_pred) 
+print("Precision score:",precision)
+recall = metrics.recall_score(y_test, y_pred) 
+print("Recall score:",recall)
+
+# Plotting the Confusion Matrix
+plt.figure(figsize=(8, 6))
+sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
+plt.title("Confusion Matrix")
+plt.xlabel("Predicted Labels")
+plt.ylabel("True Labels")
+plt.show()
+
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier()
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)  
+
+from sklearn.metrics import confusion_matrix 
+import seaborn as sns
+cm = confusion_matrix(y_test, y_pred)  
+print(cm)
+accuracy = metrics.accuracy_score(y_test, y_pred) 
+print("Accuracy score:",accuracy)
+precision = metrics.precision_score(y_test, y_pred) 
+print("Precision score:",precision)
+recall = metrics.recall_score(y_test, y_pred) 
+print("Recall score:",recall)
+
+# Plotting the Confusion Matrix
+plt.figure(figsize=(8, 6))
+sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
+plt.title("Confusion Matrix")
+plt.xlabel("Predicted Labels")
+plt.ylabel("True Labels")
+plt.show()
